de9ffeb9290b6a34e171b5cf4e39e97d512e763c
Chanuka999 <chanukaranditha99@gmail.com>
2024-11-25 18:52:40 +0530

    update

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
     <div class="bg-info py-4">
         <h2 class="text-center">Login</h2>
         <div class="container text-center">
-            <form action="">
+            <form action="./actions/login.php" method="POST">
                 <div class="mb-3">
                     <input type="text" class="form-control w-50 m-auto" name="username" placeholder="Enter your username" require="requried">
                  </div>
@@ -27,7 +27,7 @@
                         <option value="voter">voter</option>
                     </select>
                  </div>
-                 <button type="submit" class="btn btn-dark my-4">Login</button>
+                 <button type="submit" class="btn btn-dark my-4" name="login">Login</button>
                  <p>Don't have an account?<a href="./partials/registration.php" class="text-white"> Register here</a></p>
                  
             </form>
