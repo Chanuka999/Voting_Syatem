87add8dcd98c927066213c5f3aea56627dcb8dc9
Chanuka999 <chanukaranditha99@gmail.com>
2024-11-26 13:10:35 +0530

    apply

--- partials/dashboard.php
+++ partials/dashboard.php
@@ -27,16 +27,51 @@ if($_SESSION['status']==1){
      <h1 class="my-3">Voting System</h1>
      <div class="row my-5">
         <div class="col-md-7">
-             <div class="row">
-                <div class="col-md-4">
-                    <img src="" alt="Group image">
+            <?php
+            if(isset($_SESSION['groups'])){
+                $groups = $_SESSION['groups'];
+                for($i=0;$i<count($groups);$i++){
+                    ?>
+                    <div class="row">
+                    <div class="col-md-4">
+                        <img src="../uploads/<?php echo $groups[$i]['photo'];  ?>" alt="Group image">
+                    </div>
+                    <div class="col-md-8">
+                        <strong class="text-dark h5">Group name:</strong><?php echo $groups[$i]['username'];  ?><br>
+                        <strong class="text-dark h5">Votes:</strong><?php echo $groups[$i]['votes'];  ?><br>
+                    </div>
+                 </div>
+                 
+                 <form action="../actions/voting.php" method="POST">
+                   <input type="hidden" name="groupvotes" value="<?php echo $groups[$i]['votes'];  ?>">
+                   <input type="hidden" name="groupid" value="<?php echo $groups[$i]['id'];  ?>">
+
+                   <?php
+                 if($_SESSION['status']==1){
+                    ?>
+                    <button class="bg-success my-3 text-white px-3">Voted</button>
+                    <?php
+                 }else{
+                    ?>
+                      <button  class="bg-danger my-3 text-white px-3" type="submit">Vote</button>
+                      <?php
+                 }
+                   ?>
+                 </form>
+                 <hr>
+                 <?php
+                }
+            }else{
+                ?>
+                <div class="container">
+                    <p>No groups to display</p>
                 </div>
-                <div class="col-md-8">
-                    <strong class="text-dark h5">Group name:</strong><br>
-                    <strong class="text-dark h5">Votes:</strong><br>
-                </div>
-             </div>
-             <hr>
+                <?php
+            }
+            
+              
+            ?>
+             
         </div>
         <div class="col-md-5">
             <img src="../uploads/<?php echo $data['photo'];  ?>" alt="user image">
