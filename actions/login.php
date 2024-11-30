634bb910e57f5b076a05d42b4dc915c8030bf5c0
Chanuka999 <chanukaranditha99@gmail.com>
2024-11-30 20:52:29 +0530

    update

--- actions/login.php
+++ actions/login.php
@@ -13,14 +13,14 @@ if (isset($_POST["login"])) {
     $password = $_POST["password"];
     $std = $_POST["std"];
 
-    // Fetch the user record for the given username and mobile
+   
     $sql = "SELECT * FROM `user` WHERE username = ? AND mobile = ?  AND standard = ?";
     
     $stmt= mysqli_stmt_init($conn);
   
 
     if (mysqli_stmt_prepare($stmt,$sql)) {
-        // Bind parameters to the query
+       
         mysqli_stmt_bind_param($stmt, "sss", $username, $mobile, $std);
         mysqli_stmt_execute($stmt);
 
@@ -29,9 +29,9 @@ if (isset($_POST["login"])) {
         if (mysqli_num_rows($result) > 0) {
             $user = mysqli_fetch_assoc($result);
 
-            // Verify the password
+           
             if (password_verify($password, $user['password'])) {
-                // Fetch groups
+            
                 $sql = "SELECT username, photo, votes, id FROM `user` WHERE standard = 'group'";
                 $resultGroup = mysqli_query($conn, $sql);
 
@@ -40,7 +40,7 @@ if (isset($_POST["login"])) {
                     $_SESSION['groups'] = $groups;
                 }
 
-                // Set session variables
+              
                 $_SESSION['id'] = $user['id'];
                 $_SESSION['status'] = $user['status'];
                 $_SESSION['data'] = $user;
